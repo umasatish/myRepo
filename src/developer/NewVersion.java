916315319246c6c593d5fc39dadf8cc5f2f87e1c
Satish <Satish@Dell>
2022-04-10 12:30:55 +0530

    This is my first commit

---
+++ src/developer/NewVersion.java
@@ -0,0 +1,22 @@
+package developer;
+
+public class NewVersion {
+
+	public static void main(String[] args) {
+		
+		
+		String str1="Uma";
+		String str2="Maheswari";
+
+		String str3="Satish";
+		
+		String S=str1.concat(str2).concat(str3);
+		//String S1=S.concat(str3);
+		
+		System.out.println(S);
+
+		
+		
+
+	}
+}
